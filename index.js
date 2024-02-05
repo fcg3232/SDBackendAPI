f57bad5bddff39831a9ed3b652bc89b0170f0f12
khuzi <Khuzaimayaseen@gmail.com>
2024-02-05 15:18:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require('body-parser');
-const logger = require('morgan')
-const path = require('path');
-const multer = require('multer');
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const path = require("path");
+const multer = require("multer");
 const mongoose = require("mongoose");
 const register = require("./routes/register");
 const login = require("./routes/login");
@@ -25,7 +25,7 @@ const TermsofCondition = require("./routes/TermsofCondition");
 const buyerOrder = require("./routes/buyerOrder");
 const sellerOrder = require("./routes/sellerOrder");
 const orderMatching = require("./routes/orderMatching");
-const Web3 = require('web3');
+const Web3 = require("web3");
 const { auth, isUser, isAdmin } = require("./middleware/auth");
 const { User } = require("./models/user");
 const Infura_url = process.env.Infura;
@@ -86,7 +86,6 @@ const web3 = new Web3(Infura_url);
 // 	//       console.error('Error estimating gas:', error);
 // 	//     });
 
-
 // 	// let appoint = await realEstateContract?.methods.GrantPropOwnerRole("0x7BB0c04682Bc957827fdF14a7Cc07A9C350C7C08");
 // 	// const encoded = appoint.encodeABI();
 // 	// const transactionAdmin = {
@@ -105,7 +104,6 @@ const web3 = new Web3(Infura_url);
 // 	// 	.catch(error => {
 // 	// 		console.error('Transaction error:', error);
 // 	// 	});
-
 
 // 	// web3.eth.estimateGas({
 // 	// 	from: '0x6FA348FF080e701aFbe8DbbE959576212B4213d0',
@@ -134,7 +132,6 @@ const web3 = new Web3(Infura_url);
 // 	// 	.catch(error => {
 // 	// 		console.error('Error estimating gas:', error);
 // 	// 	});
-
 
 // 	  let Propertytoken = await realEstateContract?.methods.CreateProperty('0x11fEF3F29B81491c79e433EFE2e533401Bf3cC71');
 // 	  const encodedABI = Propertytoken.encodeABI();
@@ -183,49 +180,44 @@ const app = express();
 require("dotenv").config();
 // app.use(express.json());
 
-
-
-app.use(bodyParser.json({ limit: '50000mb' }));
-app.use(bodyParser.urlencoded({ limit: '50000mb', extended: true, parameterLimit: 50000 }));
+app.use(bodyParser.json({ limit: "50000mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50000mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 
 app.use(cors());
-app.use(express.json({ extended: true, parameterLimit: 1000000000, limit: "50000mb" }));
-
-
-app.use(express.static(path.join(__dirname, "./frontend/dist")));
-
-app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "./frontend/dist/index.html"));
-});
-
-
-
+app.use(
+  express.json({ extended: true, parameterLimit: 1000000000, limit: "50000mb" })
+);
 
 //.........................file upload using multer...................................//
 
 app.use("/imges", express.static(path.join(__dirname, "/imges")));
 const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, "imges");
-	},
+  destination: (req, file, cb) => {
+    cb(null, "imges");
+  },
 
-	filename: (req, file, cb) => {
-		// let ext = path.extname(file.originalname)
-		// cb(null, Date.now() + '-' + ext)
-		// cb(null,ext)
-		cb(null, req.body.name);
-	},
+  filename: (req, file, cb) => {
+    // let ext = path.extname(file.originalname)
+    // cb(null, Date.now() + '-' + ext)
+    // cb(null,ext)
+    cb(null, req.body.name);
+  },
 });
 
 const upload = multer({ storage: storage });
 
 app.post("/api/upload", upload.single("file"), (req, res) => {
-	res.status(200).json("File has been uploaded");
-	// file.mv(`${__dirname}/imges/${file.name}`)
+  res.status(200).json("File has been uploaded");
+  // file.mv(`${__dirname}/imges/${file.name}`)
 });
 
 //.........................end...................................//
-
 
 app.use("/api/register", register);
 app.use("/api/login", login);
@@ -253,70 +245,68 @@ app.use("/api/orderMatching", orderMatching);
 
 // console.log(Property);
 app.get("/orderMatching", (req, res) => {
-	res.send(orderMatching);
+  res.send(orderMatching);
 });
 app.get("/buyerOrder", (req, res) => {
-	res.send(buyerOrder);
+  res.send(buyerOrder);
 });
 app.get("/sellerOrder", (req, res) => {
-	res.send(sellerOrder);
+  res.send(sellerOrder);
 });
 
 app.get("/TermsofCondition", (req, res) => {
-	res.send(TermsofCondition);
+  res.send(TermsofCondition);
 });
 
 app.get("/sendemail", (req, res) => {
-	res.send("sending mail");
+  res.send("sending mail");
 });
 app.get("/register", (req, res) => {
-	res.send("sending mail");
+  res.send("sending mail");
 });
-
 
 app.get("/products", (req, res) => {
-	res.send(products);
+  res.send(products);
 });
 
 app.get("/personaldb", (req, res) => {
-	res.send(personaldb);
+  res.send(personaldb);
 });
 
 app.get("/personaldb", (req, res) => {
-	res.send(personaldb);
+  res.send(personaldb);
 });
 
 app.get("/berbix", (req, res) => {
-	res.send(berbix);
+  res.send(berbix);
 });
 
 app.get("/propLLC", (req, res) => {
-	res.send(propLLC);
+  res.send(propLLC);
 });
 
 app.get("/propertyInfo", (req, res) => {
-	res.send(propertyInfo);
+  res.send(propertyInfo);
 });
 app.get("/blogdb", (req, res) => {
-	res.send(blogdb);
+  res.send(blogdb);
 });
 
 app.get("/users", (req, res) => {
-	res.send(users);
+  res.send(users);
 });
 
 const url = process.env.DB_URI;
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-	console.log(`Server running on port: ${port}...`);
+  console.log(`Server running on port: ${port}...`);
 });
 
-
-mongoose.connect(url, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-})
-	.then(() => console.log("MongoDB connection established..."))
-	.catch((error) => console.error("MongoDB connection failed:", error.message));
-
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection established..."))
+  .catch((error) => console.error("MongoDB connection failed:", error.message));
