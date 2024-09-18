07004ffb7b0a3a85abca4d88201482b3668a4896
hamza <hamza@pop-os.localdomain>
2024-09-18 21:14:36 +0500

    commented extra code causing error in logs

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require('body-parser');
-const logger = require('morgan')
-const path = require('path');
-const multer = require('multer');
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const path = require("path");
+const multer = require("multer");
 const mongoose = require("mongoose");
 const register = require("./routes/register");
 const login = require("./routes/login");
@@ -30,49 +30,50 @@ const app = express();
 require("dotenv").config();
 // app.use(express.json());
 
-
-
-app.use(bodyParser.json({ limit: '50000mb' }));
-app.use(bodyParser.urlencoded({ limit: '50000mb', extended: true, parameterLimit: 50000 }));
+app.use(bodyParser.json({ limit: "50000mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50000mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 
 app.use(cors());
-app.use(express.json({ extended: true, parameterLimit: 1000000000, limit: "50000mb" }));
+app.use(
+  express.json({ extended: true, parameterLimit: 1000000000, limit: "50000mb" })
+);
 
+// app.use(express.static(path.join(__dirname, "./frontend/dist")));
 
-app.use(express.static(path.join(__dirname, "./frontend/dist")));
-
-app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "./frontend/dist/index.html"));
-});
-
-
-
+// app.get("/", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "./frontend/dist/index.html"));
+// });
 
 //.........................file upload using multer...................................//
 
 app.use("/imges", express.static(path.join(__dirname, "/imges")));
 const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, "imges");
-	},
+  destination: (req, file, cb) => {
+    cb(null, "imges");
+  },
 
-	filename: (req, file, cb) => {
-		// let ext = path.extname(file.originalname)
-		// cb(null, Date.now() + '-' + ext)
-		// cb(null,ext)
-		cb(null, req.body.name);
-	},
+  filename: (req, file, cb) => {
+    // let ext = path.extname(file.originalname)
+    // cb(null, Date.now() + '-' + ext)
+    // cb(null,ext)
+    cb(null, req.body.name);
+  },
 });
 
 const upload = multer({ storage: storage });
 
 app.post("/api/upload", upload.single("file"), (req, res) => {
-	res.status(200).json("File has been uploaded");
-	// file.mv(`${__dirname}/imges/${file.name}`)
+  res.status(200).json("File has been uploaded");
+  // file.mv(`${__dirname}/imges/${file.name}`)
 });
 
 //.........................end...................................//
-
 
 app.use("/api/register", register);
 app.use("/api/login", login);
@@ -100,70 +101,68 @@ app.use("/api/orderMatching", orderMatching);
 
 // console.log(Property);
 app.get("/orderMatching", (req, res) => {
-	res.send(orderMatching);
+  res.send(orderMatching);
 });
 app.get("/buyerOrder", (req, res) => {
-	res.send(buyerOrder);
+  res.send(buyerOrder);
 });
 app.get("/sellerOrder", (req, res) => {
-	res.send(sellerOrder);
+  res.send(sellerOrder);
 });
 
 app.get("/TermsofCondition", (req, res) => {
-	res.send(TermsofCondition);
+  res.send(TermsofCondition);
 });
 
 app.get("/sendemail", (req, res) => {
-	res.send("sending mail");
+  res.send("sending mail");
 });
 app.get("/register", (req, res) => {
-	res.send("sending mail");
+  res.send("sending mail");
 });
-
 
 app.get("/products", (req, res) => {
-	res.send(products);
+  res.send(products);
 });
 
 app.get("/personaldb", (req, res) => {
-	res.send(personaldb);
+  res.send(personaldb);
 });
 
 app.get("/personaldb", (req, res) => {
-	res.send(personaldb);
+  res.send(personaldb);
 });
 
 app.get("/kyc", (req, res) => {
-	res.send(kyc);
+  res.send(kyc);
 });
 
 app.get("/propLLC", (req, res) => {
-	res.send(propLLC);
+  res.send(propLLC);
 });
 
 app.get("/propertyInfo", (req, res) => {
-	res.send(propertyInfo);
+  res.send(propertyInfo);
 });
 app.get("/blogdb", (req, res) => {
-	res.send(blogdb);
+  res.send(blogdb);
 });
 
 app.get("/users", (req, res) => {
-	res.send(users);
+  res.send(users);
 });
 mongoose.set("strictQuery", false);
 const url = process.env.DB_URI;
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-	console.log(`Server running on port: ${port}...`);
+  console.log(`Server running on port: ${port}...`);
 });
 
-
-mongoose.connect(url, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-})
-	.then(() => console.log("MongoDB connection established..."))
-	.catch((error) => console.error("MongoDB connection failed:", error.message));
-
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection established..."))
+  .catch((error) => console.error("MongoDB connection failed:", error.message));
