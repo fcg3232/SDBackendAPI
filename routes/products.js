267e38d143bd04e493c5565a9a6abf067c0a4035
umair riaz <umairriaz789@gmail.com>
2024-10-10 16:31:51 +0500

    updated

--- routes/products.js
+++ routes/products.js
@@ -7,12 +7,63 @@ const { User } = require("../models/user");
 
 const Infura_url = process.env.Infura;
 const ContractAddress = process.env.CONTRACT_ADDRESS;
-// const SEDTAddress = process.env.PropertyToken_ADDRESS;
+const walletAddresss = process.env.address ;
 const web3 = new Web3(Infura_url);
 const ContractABI = require("../contract/SecondaryDAO.json");
 const EscrowABI = require("../contract/Escrow.json");
 const PropertyABI = require("../contract/Property.json");
+const USDCaddr = "0x0153002d20B96532C639313c2d54c3dA09109309";
+const USDTaddr = "0x80EDee6f667eCc9f63a0a6f55578F870651f06A4";
 
+const aggregatorV3InterfaceABI = [
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "description",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint80", name: "_roundId", type: "uint80" }],
+    name: "getRoundData",
+    outputs: [
+      { internalType: "uint80", name: "roundId", type: "uint80" },
+      { internalType: "int256", name: "answer", type: "int256" },
+      { internalType: "uint256", name: "startedAt", type: "uint256" },
+      { internalType: "uint256", name: "updatedAt", type: "uint256" },
+      { internalType: "uint80", name: "answeredInRound", type: "uint80" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "latestRoundData",
+    outputs: [
+      { internalType: "uint80", name: "roundId", type: "uint80" },
+      { internalType: "int256", name: "answer", type: "int256" },
+      { internalType: "uint256", name: "startedAt", type: "uint256" },
+      { internalType: "uint256", name: "updatedAt", type: "uint256" },
+      { internalType: "uint80", name: "answeredInRound", type: "uint80" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "version",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+]
 // updated token price
 router.get("/update/:id", isAdmin, async (req, res) => {
   const EscrowContract = new web3.eth.Contract(EscrowABI, req.params.id);
@@ -75,7 +126,7 @@ router.post("/:id", isAdmin, async (req, res) => {
           .then(async (receipt) => {
             console.log('Transaction receipt:', receipt);
             let PropertyAdd = await realEstateContract?.methods.PropertyAddress().call({ from: AdminWallet.walletAddress });
-            console.error('Property Address:', PropertyAdd);
+            console.error('Property Address:', receipt);
 
             if (image) {
               const uploadedResponse = await cloudinary.uploader.upload(image, {
@@ -150,23 +201,27 @@ router.patch("/order/:id", async (req, res) => {
     const fromUser = await User.findOne({ walletAddress: ord.AdminWallet });
     const Contract = new web3.eth.Contract(PropertyABI, ord.uid);
     if (!Contract) return res.status(404).send("Contract not found...");
-    const address = await Contract?.methods.EscrowAccount().call({ from: ord.AdminWallet });;
+    const address = await Contract?.methods.EscrowContractAddress().call({ from: ord.AdminWallet });;
     if (!address) return res.status(404).send("address not found...");
     const EscrowContract = new web3.eth.Contract(EscrowABI, address);
+    const gasPrice = await web3.eth.getGasPrice();
     if (!EscrowContract) return res.status(404).send("Escrow Contract not found...");
     const buy = await EscrowContract?.methods.BuyPropertyToken(
       req.body.buyer,
       req.body.currency,
       req.body.tokens,
+      req.body.Receivabletokens,
+      req.body.Receivablefee,
     );
     const encodedABI = buy.encodeABI();
+    console.log('encodedABI:', encodedABI);
+    console.log('address:', address);
     web3.eth.estimateGas({
       from: ord.AdminWallet,
       to: address,
       data: encodedABI
     })
       .then(gasEstimate => {
-        console.log('Gas estimate:', gasEstimate);
         const transactionAdmin = {
           from: ord.AdminWallet, // Replace with your Ethereum address
           to: address, // Replace with your smart contract address
@@ -178,10 +233,19 @@ router.patch("/order/:id", async (req, res) => {
             return web3.eth.sendSignedTransaction(signedTx.rawTransaction);
           }).then(async (receipt) => {
             console.log('Transaction receipt:', receipt);
-
+            const Gas = gasEstimate*gasPrice/1e18;
             const updatedord = await Product.findByIdAndUpdate(
               req.params.id,
-              { $push: { ...req.body} },
+              // { $push: { ...req.body} },
+              { $push: { 'tokenHolder': {
+                'buyer': req.body.buyer,
+                'TypeOfCurrency': req.body.currency,
+                'TokensOrder': req.body.tokens,
+                'PayableTokens': req.body.Receivabletokens,
+                'PayableFee':req.body.Receivablefee,
+                "Total Used Gas": Gas,
+                "transactionHash":receipt.transactionHash,
+              }} },
               // req.body,
               // {
               //   $set: {
@@ -197,8 +261,9 @@ router.patch("/order/:id", async (req, res) => {
             res.send(updatedord);
           })
       })
+   
   } catch (error) {
-    res.status(500).send("Error: " + error.message);
+    res.status(500).send("Error on trasect: " + error.message);
     console.log(error.message);
   }
 })
@@ -271,8 +336,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-//GET PRODUCT
-
+//GET Property Details from mongoDB
 router.get("/find/:id", async (req, res) => {
   try {
     const product = await Product.findById(req.params.id);
@@ -282,14 +346,72 @@ router.get("/find/:id", async (req, res) => {
   }
 });
 
-// router.get("/find/:uid", async (req, res) => {
-//   try {
-//     // const uidfind = req.params.uid
-//     const productss = await Product.findOne(req.params.id);
-//     res.status(200).send(productss);
-//   } catch (error) {
-//     res.status(500).send(error);
-//   }
-// });
+
+// fetch the property details from Blcokchain
+router.get("/fetch/:id", async (req, res) => {
+  try {
+    const ord = await Product.findById(req.params.id);
+    if (!ord) return res.status(404).send("Not found...");
+    const PropContract = new web3.eth.Contract(
+      PropertyABI, ord.uid);
+      if (!PropContract) return res.status(404).send("Contracts not found...");
+      let PropertyDetails = await PropContract?.methods.
+      getCompletePropDetails().call({ 
+        from: walletAddresss });
+    res.status(200).send(PropertyDetails);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+
+  
+router.get("/price/:id", async (req, res) => {
+  try {
+    const ord = await Product.findById(req.params.id);
+    if (!ord) return res.status(404).send("Not found...");
+    const Contract = new web3.eth.Contract(PropertyABI, ord.uid);
+    if (!Contract) return res.status(404).send("Contract not found...");
+    let PropertyDetails = await Contract?.methods.TokenPrice().call({ 
+      from: walletAddresss });
+    res.status(200).send(PropertyDetails);
+  } catch (error) {
+    res.status(500).send(error); 
+  }
+});
+
+router.get("/usdt/:id", async (req, res) => {
+  try {
+    const ord = await Product.findById(req.params.id);
+    if (!ord) return res.status(404).send("Not found...");
+    const Contract = new web3.eth.Contract(
+      aggregatorV3InterfaceABI, 
+      USDTaddr
+      );
+    if (!Contract) return res.status(404).send("Contract not found...");
+    let PropertyDetails = await Contract?.methods.latestRoundData().call({ 
+      from: ord.AdminWallet });
+    res.status(200).send(PropertyDetails.answer);
+  } catch (error) {
+    res.status(500).send(error); 
+  }
+});
+
+router.get("/usdc/:id", async (req, res) => {
+  try {
+    const ord = await Product.findById(req.params.id);
+    if (!ord) return res.status(404).send("Not found...");
+    const Contract = new web3.eth.Contract(
+      aggregatorV3InterfaceABI, 
+      USDCaddr
+      );
+    if (!Contract) return res.status(404).send("Contract not found...");
+    let PropertyDetails = await Contract?.methods.latestRoundData().call({ 
+      from: ord.AdminWallet });
+    res.status(200).send(PropertyDetails.answer);
+  } catch (error) {
+    res.status(500).send(error); 
+  }
+});
 
 module.exports = router;
