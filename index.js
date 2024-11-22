73b635d4e831d7558257af96016dec74e8ab90b2
phelix001 <freedman.joshua@gmail.com>
2024-11-22 10:40:43 -0500

    Add CORS middleware to dynamically set allowed origins and headers

--- index.js
+++ index.js
@@ -71,8 +71,21 @@ app.use(
   })
 );
 
-
-
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, content-type, authorization"
+  );
+  next();
+});
 
 const calculateAnnualCoC = async () => {
   const products = await Product.find();
