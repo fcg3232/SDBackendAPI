20ef59652befb20c21556b290f0b132f4fa744f1
umair riaz <umairriaz789@gmail.com>
2024-11-27 12:47:16 +0500

    updated

--- index.js
+++ index.js
@@ -552,7 +552,7 @@ app.get("/", (req, res) => {
 
 mongoose.set("strictQuery", false);
 const url = process.env.DB_URI;
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}...`);
