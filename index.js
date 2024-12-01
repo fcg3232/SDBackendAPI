fe7831c9ab64792f5588bdd91ba5977b62ba54b7
hamza <hamza@pop-os.localdomain>
2024-12-02 02:12:03 +0500

    undo changes

--- index.js
+++ index.js
@@ -65,19 +65,6 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, content-type, authorization"
-  );
-  next();
-});
-
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
